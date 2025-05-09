6e95d48e4990b0d613627d00158289882bac0d51
angeleduiglesias <angeledu.igle.22@gmail.com>
2025-05-08 21:53:44 -0500

    conexion establecida del dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,20 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cliente;
+use App\Models\Tramite;
+use App\Models\Documento;
+use App\Models\Notario;
+use App\Models\Empresa;
+use App\Models\Pago;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(IndexAdminRequest $request)
+    public function index(Request $request)
     {
-        // Validate the request
-        $validated = $request->validated();
+        $user = auth()->user();
+
+        // Verifica si el usuario es admin
+        if ($request->user()->rol !== 'admin') {
+            return response()->json(['message' => 'No tienes permisos'], 403);
+        }
 
         // Fetch data from the database
         $clientes = Cliente::all();
-        $tramites = Tramite::all();
         $documentos = Documento::all();
         $notarios = Notario::all();
         $empresas = Empresa::all();
@@ -24,10 +35,10 @@ class AdminController extends Controller
         //card
         $clientes_registrados = $clientes->count();
         $clientes_activos = $clientes->where('estado', 'activo')->count();
-        $tramites_pendientes = $tramites->where('estado', 'pendiente')->count();
+        $tramites_pendientes = Tramite::where('estado', 'pendiente')->count();
 
         // mostrar los tramites recientes pero con solo 15 registros
-        $tramites_recientes = $tramites->where('created_at', '>=', now()->subDays(30))->take(15)->get();
+        $tramites_recientes = Tramite::where('created_at', '>=', now()->subDays(30))->take(15)->get();
 
         //mostrar los pagos recientes pero con solo 10 registros
         $pagos_recientes = Pago::where('created_at', '>=', now()->subDays(30))->take(10)->get();
@@ -35,7 +46,6 @@ class AdminController extends Controller
         // Return a JSON response with the data
         return response()->json([
             $clientes,
-            $tramites,
             $documentos,
             $notarios,
             $empresas,
